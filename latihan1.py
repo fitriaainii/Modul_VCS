6a9d524330aa7836be5c193bd8b2e5b577f6b7ae
fitriaainii <114564745+fitriaainii@users.noreply.github.com>
2025-03-10 10:50:29 +0700

    Add files via upload

---
+++ latihan1.py
@@ -0,0 +1,24 @@
+nama = "Fitria Nur Aini"
+gender = "P"
+umur = 21
+pekerjaan = "Mahasiswa"
+print(
+    "Nama Mahasiswa:", nama,
+    "\nJenis Kelamin:", gender,
+    "\nUmur:", umur,"tahun",
+    "\nPekerjaan:", pekerjaan
+)
+
+
+# belajar operator aritmatika
+# Luas segita siku-siku
+matkul = "Belajar Menghitung Luas Segita Siku-Siku"
+lembaga = "\ndengan Fitria"
+title = matkul + lembaga
+alas = 7
+tinggi = 7
+luas = 0.5 * alas * tinggi
+
+print(title)
+print("\nLuas Segita Siku-Siku",luas, "cm")
+
